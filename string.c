f96b0b8ee76ef83245b66f8bf3acfc01fd375621
Jahid Hasan <jahidsite0@gmail.com>
2024-06-13 08:05:48 +0600

    Day 4 class ended!

--- string.c
+++ string.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    char arafat[14] = "Arafat Hossen";
-    printf("%s\n", arafat);
-    printf("\n%c", arafat[4]);
+    // char arafat[14] = "Arafat Hossen";
+    // printf("%s\n", arafat);
+    // printf("\n%c", arafat[4]); '\0'
+
+    //char name[6] = {'R', 'a', 's', 'h', 'e', 'd'};
+    char name[10] = "Rashed\0"; //{'R', 'a', 's', 'h', 'e', 'd'};
+
+    printf("string name: %s\n", name);
+
     return 0;
 }
